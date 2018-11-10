598797924435e88fc87c75b7fa86409d3aea44ee
Ariv <arivbits@gmail.com>
2018-11-10 10:21:30 +0530

    First commit

---
+++ src/main/java/com/shab/artificon/ArtificOnCrawlerApplication.java
@@ -0,0 +1,12 @@
+package com.shab.artificon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtificOnCrawlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtificOnCrawlerApplication.class, args);
+	}
+}
