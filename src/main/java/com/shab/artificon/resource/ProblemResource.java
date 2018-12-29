862a2e4a6e80dfa43f13254ca62467764124adf0
Ariv <arivbits@gmail.com>
2018-12-29 17:38:29 +0530

    Upload API

--- src/main/java/com/shab/artificon/resource/ProblemResource.java
+++ src/main/java/com/shab/artificon/resource/ProblemResource.java
@@ -3,17 +3,25 @@
  */
 package com.shab.artificon.resource;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.shab.artificon.model.ProblemDto;
 import com.shab.artificon.repository.ProblemRepository;
+import com.shab.artificon.utils.CSVReaderUtils;
 
 /**
  * @author zentere
@@ -25,6 +33,13 @@ public class ProblemResource {
 
 	@Autowired
 	private ProblemRepository problemRepository;
+	
+	@PostMapping("/uploadCsv")
+	public String uploadDocxFile(@RequestParam("file") MultipartFile file) throws FileNotFoundException, IOException {
+		List<ProblemDto> problems = CSVReaderUtils.convertCsvToBean(convert(file).getAbsolutePath());
+		problemRepository.saveAll(problems);
+		return "Success";
+	}
 
 	@GetMapping("problems")
 	public List<ProblemDto> getAllProblems() {
@@ -41,5 +56,14 @@ public class ProblemResource {
 			steps.add(problemDto.getSolution());
 		}
 		return steps;
+	}
+	
+	public static File convert(MultipartFile file) throws IOException {
+		File convFile = new File(file.getOriginalFilename());
+		convFile.createNewFile();
+		FileOutputStream fos = new FileOutputStream(convFile);
+		fos.write(file.getBytes());
+		fos.close();
+		return convFile;
 	}
 }
