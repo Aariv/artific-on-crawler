0fd8932b76f54be9a365c705e38c390deba94fbc
Ariv <arivbits@gmail.com>
2018-12-29 17:43:16 +0530

    Delete all api

--- src/main/java/com/shab/artificon/resource/ProblemResource.java
+++ src/main/java/com/shab/artificon/resource/ProblemResource.java
@@ -11,6 +11,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -56,6 +57,12 @@ public class ProblemResource {
 			steps.add(problemDto.getSolution());
 		}
 		return steps;
+	}
+	
+	@DeleteMapping("problems")
+	public String deleteAllProblem() {
+		problemRepository.deleteAll();
+		return "Success";
 	}
 	
 	public static File convert(MultipartFile file) throws IOException {
